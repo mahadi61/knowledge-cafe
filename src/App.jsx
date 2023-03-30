5b8d6449bf20f6bddf275529837d003c30cd3a5e
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-03-30 23:30:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./Component/Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <hr className="hr-line" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="nav-bar">
+      <span className="nav-title">Info House</span>
+      <img
+        className="nav-img"
+        src="https://img.freepik.com/free-photo/young-beautiful-woman-pink-warm-sweater-natural-look-smiling-portrait-isolated-long-hair_285396-896.jpg?w=900&t=st=1680197048~exp=1680197648~hmac=dd8308a155a687192f7581f7d2124706fbb9b0b7706fb65b2644380176009764"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Header;
